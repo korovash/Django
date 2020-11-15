d9607db34c15577c53a74f666153cb2f11fbd35d
Рябинин Павел Евгеньевич <riabinin.pe@dvfu.ru>
2020-11-15 21:23:37 +1000

    fix images

--- printmon/settings.py
+++ printmon/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = int(os.getenv('DEBUG'))
-# DEBUG = 1
+#DEBUG = 1
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(" ")
 
